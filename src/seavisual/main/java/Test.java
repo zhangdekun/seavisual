ae03cae895e95abec5f31f0a3a549c2f89247b97
zhangdekun <gl.dekun.zhang@gmail.com>
2014-07-13 18:01:33 +0800

    update

---
+++ src/seavisual/main/java/Test.java
@@ -0,0 +1,17 @@
+package seavisual.main.java;
+
+/**
+ * @author zhangdekun
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world...");
+	}
+
+}
